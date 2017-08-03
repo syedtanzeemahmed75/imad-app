b642e1dfa4d41771339bda0e9bc4eea7b3344f63
syedtanzeemahmed75 <syedtanzeemahmed75@gmail.com>
2017-08-03 06:14:58 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
