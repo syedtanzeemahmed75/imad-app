ebbdb57f12468feda70d3d25412d6d6f4a49ac39
syedtanzeemahmed75 <syedtanzeemahmed75@gmail.com>
2017-08-03 06:19:21 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req , res){
-  res.send('article 1 is requested and will be served');
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
 });
 app.get('/article-two',function(req , res){
   res.send('article 2 is requested and will be served');
